c5446bffc4f6beeb32fc97364d6f0ad2fffd8848
Bruce Rannala <brannala@ucdavis.edu>
2020-04-05 00:40:51 -0700

    Added getopt code for command line parameter specification

--- src/coalsim.c
+++ src/coalsim.c
@@ -6,12 +6,81 @@
 
 gsl_rng * r;
 
+/* options */
+int opt_help = 0; /* print help message */
+int opt_default = 0; /* print help message */
 
 char version[] = "coalsim";
 
-int main()
+static void print_msg()
 {
+  printf("Usage: coalsim [OPTION]... \n Try 'coalsim -h' for more information.\n");
+}
+
+static void print_help()
+{
+  printf("Coalescent simulations: \n"
+	 "-c <sample size>\n"
+	 "-N <population size>\n"
+	 "-r <recombination rate>\n"
+	 "-s <seed for RNG>\n"
+	 "-m <mutation rate>\n");
+}
+
+int main(int argc, char **argv)
+{
+  fillheader(version);
+  show_header();
+  double popSize = 10000;
+  double recRate = 0.1;
+  double mutRate = 3.0;
+  unsigned int noSamples=5;
+  unsigned int RGSeed=0;
+  char* endPtr;
+  opterr = 0;
+  int c;
   const gsl_rng_type * T;
+
+  while((c = getopt(argc, argv, "c:N:r:m:s:h")) != -1)
+    switch(c)
+      {
+      case 'c':
+	noSamples = strtoul(optarg,&endPtr,10);
+	break;
+      case 'N':
+	popSize = atof(optarg);
+	break;
+      case 'r':
+	recRate = atof(optarg);
+	break;
+      case 'm':
+	mutRate = atof(optarg);
+	break;
+      case 's':
+	RGSeed = strtoul(optarg,&endPtr,10);
+	break;
+      case 'h':
+	opt_help = 1;
+	break;
+      case '?':
+        if (isprint (optopt))
+          fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+        else
+          fprintf(stderr,"Unknown option character `\\x%x'.\n",optopt);
+        return 1;
+      default:
+	abort();
+      }
+  if(optind == 1)
+    {
+      print_msg();
+      return 1;
+    }
+  if(opt_help)
+    {
+      print_help();
+      return 1;
+    }
 
  /* create a generator chosen by the
     environment variable GSL_RNG_TYPE */
@@ -20,10 +89,11 @@ int main()
   // gsl_rng_default_seed = 45567; 
   T = gsl_rng_default;
   r = gsl_rng_alloc (T);
-
+  if(RGSeed != 0)
+    gsl_rng_set(r,RGSeed);
+  
+  unsigned int noChrom = noSamples;
   recombination_event recombEvent;
-  unsigned int noChrom=20;
-  unsigned int noSamples=20;
   chromosome* currentChrom = NULL;
   chrsample* chromSample = create_sample(noChrom);
   ancestry* tmp = NULL;
@@ -33,9 +103,6 @@ int main()
   double totalTime=0;
   double interArrivalTime=0;
   currentChrom = chromSample->chrHead;
-  double popSize = 10000;
-  double recRate = 0.1;
-  double mutRate = 3.0;
   double totRate=0;
   double coalProb = 0;
   double recProb = 0;
@@ -63,7 +130,7 @@ int main()
     assert(coalProb + recProb < 1.0);
     interArrivalTime = gsl_ran_exponential(r, 1.0/totRate);
     if(!(eventNo % 5000))
-      printf("interArrivaltime: %lf NoRec: %d NoCoal: %d\n",interArrivalTime,noRec,noCoal);
+      printf("interArrivaltime: %lf totalTime: %lf NoMut: %d NoRec: %d NoCoal: %d\n",interArrivalTime,totalTime,noMutations,noRec,noCoal);
     
     totalTime += interArrivalTime;
     prob = gsl_rng_uniform_pos(r);
@@ -85,29 +152,7 @@ int main()
 	}
       else
 	noMutations++;
-
-    /*    printf("recNo: %d\n",noRec);
-    currChr=0;
-    currentChrom = chromSample->chrHead; 
-    while(currentChrom != NULL)
-      {
-	printf("\nChr: %d Anc: ",currChr);
-	tmp = currentChrom->anc;
-	while(tmp != NULL)
-	  {
-	    displayBits(tmp->abits,noSamples);
-	    printf(" %lf ",tmp->position);  
-	    tmp = tmp->next;
-	  } 
-	currentChrom = currentChrom->next;
-	currChr++;
-	} */
-    
   } 
-  /*  ancLength = totalAncLength(chromSample);
-  eventLocation = ancLength*gsl_rng_uniform_pos(r);
-  getRecEvent(chromSample, eventLocation, &recombEvent);
-  recombination(&noChrom,recombEvent,chromSample); */
   printf("recNo: %d\n",noRec);
   printf("mutNo: %d\n",noMutations);
   currChr=0;
@@ -125,31 +170,6 @@ int main()
       currentChrom = currentChrom->next;
       currChr++;
     }
-
-
-
-  
-  // coalescence(&noChrom, chromSample);
-  /* currChr=0;
-  currentChrom = chromSample->chrHead; 
-  while(currentChrom != NULL)
-    {
-      printf("\nChr: %d Anc: ",currChr);
-      tmp = currentChrom->anc;
-      while(tmp != NULL)
-	{
-	  displayBits(tmp->abits,noSamples);
-	  printf(" %lf ",tmp->position);  
-	  tmp = tmp->next;
-	  } 
-      currentChrom = currentChrom->next;
-      currChr++;
-      }
-  printf("\nTotalanclength: %lf",totalAncLength(chromSample));
-  printf("\nNo Rec: %d",noRec);
-  printf("\nNoChrom: %d\n",noChrom); */
-  //  printf("U(0,1): %lf\n",gsl_rng_uniform_pos(r));
-  // printf("U(0,1): %lf",gsl_ran_flat(r,0,1.0));
   delete_sample(chromSample->chrHead);
   free(chromSample); 
 }
